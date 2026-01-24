6f5e480b2dd147784f6706ffd00a7ef797737124
Sunggin <sungginkim528@gmail.com>
2026-01-24 15:02:16 +0900

    my solution to collections grid section challenge

--- Challenges/text-input-component/src/App.jsx
+++ Challenges/text-input-component/src/App.jsx
@@ -1,52 +1,10 @@
 import React from "react";
-import TextInput from "./components/text-input/TextInput";
-import Icons from "./assets/icons";
 import "./App.css";
 import CollectionsGrid from "./sections/collections-grid/CollectionsGrid";
-
-const PLACEHOLDER = "name@email.com";
-const HINTTEXT = "This is a hint text.";
-const ERROR_MESSAGE = "This is an error message";
-const EmailIcon = Icons.email;
-const QuestionIcon = Icons.question;
+import gridData from "./sections/collections-grid/collections.json";
 
 const App = () => {
-  return (
-    <CollectionsGrid />
-    // <div className="form">
-    //   <TextInput
-    //     label="Email"
-    //     name="email"
-    //     placeholder={PLACEHOLDER}
-    //     hintText={HINTTEXT}
-    //     trailingIcon={QuestionIcon}
-    //   />
-    //   <TextInput
-    //     label="Email"
-    //     name="email"
-    //     placeholder={PLACEHOLDER}
-    //     hintText={HINTTEXT}
-    //     leadingIcon={EmailIcon}
-    //     trailingIcon={QuestionIcon}
-    //   />
-    //   <TextInput
-    //     label="Email"
-    //     name="email"
-    //     placeholder={PLACEHOLDER}
-    //     hintText={HINTTEXT}
-    //     trailingIcon={QuestionIcon}
-    //     disabled={true}
-    //   />
-    //   <TextInput
-    //     label="Email"
-    //     name="email"
-    //     placeholder={PLACEHOLDER}
-    //     hintText={HINTTEXT}
-    //     errorMessage={ERROR_MESSAGE}
-    //     trailingIcon={QuestionIcon}
-    //   />
-    // </div>
-  );
+  return <CollectionsGrid gridData={gridData} />;
 };
 
 export default App;
--- Challenges/text-input-component/src/sections/collections-grid/CollectionsGrid.jsx
+++ Challenges/text-input-component/src/sections/collections-grid/CollectionsGrid.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import "./styles.css";
 
-const CollectionsGrid = () => {
-  return <div>CollectionsGrid</div>;
+const CollectionsGrid = ({ gridData }) => {
+  return (
+    <section>
+      <h1>Our Collections</h1>
+      <div className="grid-container">
+        {gridData.map(
+          (
+            { collection_id, name, description, image_url, created_at },
+            index,
+          ) => (
+            <div
+              key={collection_id}
+              className={`grid box-${index}`}
+              style={{ backgroundImage: `url(${image_url})` }}
+            >
+              <p className="name">{name}</p>
+              <p className="description">{description}</p>
+            </div>
+          ),
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default CollectionsGrid;
